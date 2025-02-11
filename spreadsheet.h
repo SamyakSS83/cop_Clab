23bd259549608c09d0a42311a7b37503d95df91b
ShivankurGupta <152327469+ShivankurGupta@users.noreply.github.com>
2025-02-12 02:10:00 +0530

    Update spreadsheet.h, added some parse functions

--- spreadsheet.h
+++ spreadsheet.h
@@ -15,7 +15,8 @@ typedef struct Spreadsheet {
 #ifdef __cplusplus
 extern "C" {
 #endif
-
+int spreadsheet_parse_cell_name(const Spreadsheet *sheet, const char *cell_name, int *out_row, int *out_col);
+char** spreadsheet_parse_range(const Spreadsheet *sheet, const char *range_str, int *count);
 int spreadsheet_evaluate_function(Spreadsheet *sheet, const char *func, const char *args, Cell* cell);
 int spreadsheet_evaluate_expression(Spreadsheet *sheet, const char *expr, Cell* cell);
 
