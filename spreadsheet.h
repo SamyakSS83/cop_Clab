f27b4b5db14c1d3776f8f7b9bbd39411cafe7221
Vanshika288 <153622777+Vanshika288@users.noreply.github.com>
2025-03-01 23:32:12 +0530

    Update spreadsheet.h
    updated the definitions of two newly added functions

--- spreadsheet.h
+++ spreadsheet.h
@@ -24,7 +24,7 @@ char* spreadsheet_col_to_letter(int col, char *buffer, size_t size);
 int spreadsheet_letter_to_col(const char *letters);
 int col_to_index(const char *col);
 void index_to_col(int index, char *col);
-
+int find_depends(const char *formula, Spreadsheet *sheet, int *r1, int *r2, int *c1, int *c2, int *range_bool);
 
 char* spreadsheet_get_cell_name( int row, int col, char *buffer, size_t size);
 int spreadsheet_parse_cell_name(const Spreadsheet *sheet, const char *cell_name, int *out_row, int *out_col);
@@ -32,6 +32,8 @@ int isNumeric(const char *str);
 
 int spreadsheet_evaluate_function(Spreadsheet *sheet, const char *func, const char *args, Cell* cell,const char*expr);
 int spreadsheet_evaluate_expression(Spreadsheet *sheet, const char *expr, Cell* cell);
+
+int first_step_find_cycle(Spreadsheet *sheet, char *cell_name, int r1,int r2 ,int c1,int c2,int range_bool);
 
 void spreadsheet_set_cell_value(Spreadsheet *sheet, char *cell_name, const char *formula, char *status_out, size_t status_size);
 void spreadsheet_display(Spreadsheet *sheet);
