2fe81d6f2babd1e675ed190fc0294594e8843351
ShivankurGupta <152327469+ShivankurGupta@users.noreply.github.com>
2025-03-01 23:03:41 +0530

    Updated spreadsheet.c
    Added eval_expression and eval_function

--- spreadsheet.c
+++ spreadsheet.c
@@ -237,4 +237,367 @@ int isNumeric(const char *str) {
 
     return (*endptr == '\0');  // If endptr points to '\0', it's a valid integer
 }
-   
+
+/* ----------------
+   Expression & Function
+   ---------------- */
+int spreadsheet_evaluate_function(Spreadsheet *sheet, const char *func, const char *args, Cell *cell,const char*expr)
+{
+    // fprintf(stderr, "[DEBUG] Evaluating function: %s(%s)\n", func, args);
+    // SLEEP(value)
+    if (strcasecmp(func, "SLEEP") == 0)
+    {
+        int val;
+        // case 1 , args is not cell reference
+        if (isNumeric(args))
+        {
+            val = atoi(args);
+            cell->error = 0;
+        }
+
+        else
+        {
+            int sign = 1;
+            if (args[0] == '-')
+            {
+                sign = -1;
+                args++;
+            }
+            else if (args[0] == '+')
+            {
+                args++;
+            }
+            // Cell *op = ordereddict_get(sheet->cells, args);
+            int r_, c_;
+            spreadsheet_parse_cell_name(sheet, args, &r_, &c_);
+            Cell *op = sheet->cells[sheet->cols * (r_ - 1) + (c_ - 1)];
+            if (!op)
+            {
+                fprintf(stderr, "cell not found\n");
+
+                // no formula ever assigned to this cell . it must have zero value iff it's a valid cell
+                // to be checked for a valid cell logic is inside extract already
+                val = 0;
+            }
+            else
+                val = sign * op->value;
+            if (op->error)
+            {
+                cell->error = 1;
+                return val;
+            }
+            else
+            {
+                cell->error = 0;
+            }
+        }
+        // int val = spreadsheet_evaluate_expression(sheet, args);
+        if(val>0){
+            sleep((unsigned int)val);
+        }
+
+        return val;
+    }
+
+    // Evaluate range
+    int count = 0;
+    int r1,r2,c1,c2,range_bool;
+    find_depends(expr,sheet,&r1,&r2,&c1,&c2,&range_bool);
+    count = (r2-r1+1)*(c2-c1+1);
+    // if (count == 0)
+    // {
+    //     if (cells)
+    //         free(cells);
+    //     return 0;
+    // }
+    int *values = (int *)malloc(sizeof(int) * count);
+    // fprintf(stderr,"find_depends in which index %d %d %d %d \n",r1,r2,c1,c2);
+    int k = 0;
+    for (int i = r1; i <= r2; i++)
+    {
+        for(int j=c1;j<=c2;j++){
+            // Cell *c = ordereddict_get(sheet->cells, cells[i]);
+            // int r_, c_;
+            // spreadsheet_parse_cell_name(sheet, cells[i], &r_, &c_);
+
+            Cell *c = sheet->cells[sheet->cols * (i-1 ) + (j-1)];
+            if (c && c->error)
+            {
+                cell->error = 1;
+                // free(cells);
+                free(values);
+                return 0;
+            }
+            values[k] = c ? c->value : 0;
+            k++;
+            // fprintf(stderr,"values[i] is %d]\n",values[i]);
+
+            // free(cells[i]);
+        }
+    }
+    // free(cells);
+
+    if (strcasecmp(func, "MIN") == 0)
+    {
+        int minv = values[0];
+        for (int i = 1; i < count; i++)
+            if (values[i] < minv)
+                minv = values[i];
+        free(values);
+        cell->error = 0;
+        return minv;
+    }
+    else if (strcasecmp(func, "MAX") == 0)
+    {
+        int maxv = values[0];
+        for (int i = 1; i < count; i++)
+            if (values[i] > maxv)
+                maxv = values[i];
+        free(values);
+        cell->error = 0;
+        return maxv;
+    }
+    else if (strcasecmp(func, "SUM") == 0)
+    {
+        int sumv = 0;
+        for (int i = 0; i < count; i++)
+            sumv += values[i];
+        free(values);
+        cell->error = 0;
+        // fprintf(stderr,"returns what sum %d\n",sumv);
+        return sumv;
+    }
+    else if (strcasecmp(func, "AVG") == 0)
+    {
+        int sumv = 0;
+        for (int i = 0; i < count; i++)
+            sumv += values[i];
+        free(values);
+        cell->error = 0;
+        return (count == 0) ? 0 : (sumv / count);
+    }
+    else if (strcasecmp(func, "STDEV") == 0)
+    {
+        if (count < 2)
+        {
+            free(values);
+            return 0;
+        }
+        int mean = 0;
+        for (int i = 0; i < count; i++)
+            mean += values[i];
+        mean /= count;
+        double variance = 0;
+        for (int i = 0; i < count; i++)
+        {
+            double diff = values[i] - mean;
+            variance += diff * diff;
+        }
+        variance /= (count);
+        free(values);
+        cell->error = 0;
+        return (int)round(sqrt(variance));
+    }
+
+    free(values);
+    // fprintf(stderr, "[ERROR] Unknown function: %s\n", func);
+    return 0;
+}
+
+int spreadsheet_evaluate_expression(Spreadsheet *sheet, const char *expr, Cell *cell)
+{
+    if (!expr || strlen(expr) == 0)
+        return 0;
+    // fprintf(stderr, "[DEBUG] Evaluating expression: %s\n", expr);
+
+    // Check for function call pattern: FUNC(...)
+    regex_t funcRegex;
+    regcomp(&funcRegex, "^([A-Za-z]+)\\((.*)\\)$", REG_EXTENDED);
+    regmatch_t matches[3];
+    if (regexec(&funcRegex, expr, 3, matches, 0) == 0)
+    {
+        char func[64], args[256];
+        strncpy(func, expr + matches[1].rm_so, matches[1].rm_eo - matches[1].rm_so);
+        func[matches[1].rm_eo - matches[1].rm_so] = '\0';
+        strncpy(args, expr + matches[2].rm_so, matches[2].rm_eo - matches[2].rm_so);
+        args[matches[2].rm_eo - matches[2].rm_so] = '\0';
+        regfree(&funcRegex);
+        return spreadsheet_evaluate_function(sheet, func, args, cell,expr);
+    }
+    regfree(&funcRegex);
+
+    // Check if the expr is a cell reference (e.g. A1)
+    regex_t cellRegex;
+    regcomp(&cellRegex, "^[A-Za-z]+[0-9]+$", REG_EXTENDED);
+    if (regexec(&cellRegex, expr, 0, NULL, 0) == 0)
+    {
+        // It's a cell reference
+        // Cell *c = ordereddict_get(sheet->cells, expr);
+        int r_, c_;
+        spreadsheet_parse_cell_name(sheet, expr, &r_, &c_);
+        Cell *c = sheet->cells[sheet->cols * (r_ - 1) + (c_ - 1)];
+        if (!c)
+            fprintf(stderr, "cell not found\n");
+        cell->error = c->error;
+        regfree(&cellRegex);
+        return c ? c->value : 0;
+    }
+    regfree(&cellRegex);
+
+    // Check for arithmetic
+    // We'll do a naive approach: we find + - * / and split
+
+    int temp = strlen(expr);
+    int i = 0;
+    int num1 = 0;
+    int sign1 = 1;
+    if (expr[i] == '+')
+    {
+        i++;
+    }
+    else if (expr[i] == '-')
+    {
+        i++;
+        sign1 = -1;
+    }
+    if (expr[i] >= 48 && expr[i] <= 57)
+    {
+        int j = i;
+        while (j < temp && isdigit(expr[j]))
+        {
+            num1 = num1 * 10 + (expr[j] - '0');
+            j++;
+        }
+        i = j;
+    }
+    else if (expr[i] >= 65 && expr[i] <= 90)
+    {
+        // fprintf(stderr, "goes here1\n");
+        int j = i;
+        while (j < temp && expr[j] >= 65 && expr[j] <= 90)
+        {
+            j++;
+        }
+        int k = j;
+        int row = 0;
+        while (k < temp && isdigit(expr[k]))
+        {
+            row = row * 10 + expr[k] - '0';
+            k++;
+        }
+        int temp_l = k - i;
+        char cell_name_[10];
+        strncpy(cell_name_, expr + i, temp_l);
+        cell_name_[temp_l] = '\0';
+        i = k;
+        // fprintf(stderr, "i=%d", i);
+        // Cell *c_ = ordereddict_get(sheet->cells, cell_name_);
+        int r, c;
+        spreadsheet_parse_cell_name(sheet, cell_name_, &r, &c);
+        Cell *c_ = sheet->cells[sheet->cols * (r - 1) + (c - 1)];
+        // if (!c_)
+        //     fprintf(stderr, "c_ not in dict\n");
+        if (c_->error)
+        {
+            cell->error = 1;
+            return 0;
+        }
+        num1 = c_->value;
+        // fprintf(stderr, "reaches till here\n");
+    }
+    num1 *= sign1;
+    char operation;
+    if (i < temp)
+    {
+        operation = expr[i];
+    }
+    else
+    {
+        cell->error = 0;
+        return num1;
+    }
+    i++;
+    int num2 = 0;
+    int sign2 = 1;
+    if (expr[i] == '+')
+    {
+        i++;
+    }
+    else if (expr[i] == '-')
+    {
+        sign2 = -1;
+        i++;
+    }
+    if (expr[i] >= 48 && expr[i] <= 57)
+    {
+        int j = i;
+        while (j < temp && isdigit(expr[j]))
+        {
+            num2 = num2 * 10 + (expr[j] - '0');
+            j++;
+        }
+        i = j;
+    }
+    else if (expr[i] >= 65 && expr[i] <= 90)
+    {
+        // fprintf(stderr, "should go here next\n");
+        int j = i;
+        while (j < temp && expr[j] >= 65 && expr[j] <= 90)
+        {
+            j++;
+        }
+        int k = j;
+        int row = 0;
+        while (k < temp && isdigit(expr[k]))
+        {
+            row = row * 10 + expr[k] - '0';
+            k++;
+        }
+        int temp_l = k - i;
+        char cell_name_[10];
+        strncpy(cell_name_, expr + i, temp_l);
+        cell_name_[temp_l] = '\0';
+        i = k;
+        // Cell *c = ordereddict_get(sheet->cells, cell_name_);
+        int r_, c_;
+        spreadsheet_parse_cell_name(sheet, cell_name_, &r_, &c_);
+        Cell *c = sheet->cells[sheet->cols * (r_ - 1) + (c_ - 1)];
+        // if (!c)
+        //     fprintf(stderr, "c not in dict\n");
+        if (c->error)
+        {
+            cell->error = 1;
+            return 0;
+        }
+        num2 = c->value;
+    }
+    num2 *= sign2;
+    cell->error = 0;
+    if (operation == '+')
+    {
+        return num1 + num2;
+    }
+    else if (operation == '-')
+    {
+        return num1 - num2;
+    }
+    else if (operation == '*')
+    {
+        return num1 * num2;
+    }
+    else if (operation == '/')
+    {
+        if (num2 == 0)
+        {
+            cell->error = 1;
+            return 0;
+        }
+        return num1 / num2;
+    }
+    else
+    {
+        cell->error = 1;
+        return -1;
+    }
+}
+
