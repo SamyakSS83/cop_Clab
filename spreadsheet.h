2cd9e978c6b11ff71f4ba874720e5f63384f5df6
ShivankurGupta <152327469+ShivankurGupta@users.noreply.github.com>
2025-03-01 23:08:12 +0530

    Update spreadsheet.h

--- spreadsheet.h
+++ spreadsheet.h
@@ -16,11 +16,9 @@ typedef struct Spreadsheet {
 #ifdef __cplusplus
 extern "C" {
 #endif
-static void safe_strcpy(char *dest, size_t dest_size, const char *src);
+void safe_strcpy(char *dest, size_t dest_size, const char *src);
 Spreadsheet *spreadsheet_create(int rows, int cols);
-// void spreadsheet_get_row_col(const Spreadsheet *,int *r,int *c,const char *cell_name,size_t cell_size);
-
-// Spreadsheet* spreadsheet_create(int rows, int cols);
+void destroySpreadsheet (Spreadsheet*sheet);
 
 char* spreadsheet_col_to_letter(int col, char *buffer, size_t size);
 int spreadsheet_letter_to_col(const char *letters);
@@ -30,10 +28,12 @@ void index_to_col(int index, char *col);
 
 char* spreadsheet_get_cell_name( int row, int col, char *buffer, size_t size);
 int spreadsheet_parse_cell_name(const Spreadsheet *sheet, const char *cell_name, int *out_row, int *out_col);
-char **spreadsheet_parse_range(const Spreadsheet *sheet, const char *range_str, int *count);
 int isNumeric(const char *str);
 
+int spreadsheet_evaluate_function(Spreadsheet *sheet, const char *func, const char *args, Cell* cell,const char*expr);
+int spreadsheet_evaluate_expression(Spreadsheet *sheet, const char *expr, Cell* cell);
 
+int is_valid_command(Spreadsheet *sheet, char **cell_name, char **formula);
 #ifdef __cplusplus
 }
 #endif
